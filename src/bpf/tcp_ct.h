41c07a5b4683301d3ffda129c43492606987d406
Vadim Vetrov <vetrovvd@gmail.com>
2025-06-12 11:43:43 +0300

    Simplify conntrack code

--- src/bpf/tcp_ct.h
+++ src/bpf/tcp_ct.h
@@ -176,13 +176,17 @@ static __inline int copy_ctvs_with_offset(struct cbl_cts cbts) {
 	return -(ret >= 0);
 }
 
-static __inline enum pkt_action tcp_process_conntrack(struct packet_data *pktd)
-{	
-	int ret;
+/**
+ *
+ * Builds a ct entry universal for any conntract mechanism.
+ * Note, that seq_hash is initialized with raw TCP SEQ,
+ * you will need to divide it by window later.
+ *
+ */
+static __inline struct ct_entry build_ct_entry(struct packet_data *pktd) {
 	struct ct_entry cte = {0};
 	struct ip_entry *ipe = &cte.ipe;
 	struct transport_entry *tpe = &cte.tpe; 
-	enum pkt_action act;
 
 	if (pktd->ltd.transport_type != TCP)
 		unreachable;	
@@ -196,15 +200,163 @@ static __inline enum pkt_action tcp_process_conntrack(struct packet_data *pktd)
 	}
 	
 	u32 seq = bpf_ntohl(pktd->ltd.tcph.seq);
-	u32 seqp = seq / CT_SEQ_WINSIZE;
 
 	tpe->sport = bpf_ntohs(pktd->ltd.tcph.source);
 	tpe->dport = bpf_ntohs(pktd->ltd.tcph.dest);
-	tpe->seq_hash = seqp;
+	tpe->seq_hash = seq;
 
+	return cte;
+}
 
+/**
+ * Initializes an empty ct_value. Note, that you will need to set seq manually
+ */
+static __inline void initialize_ct_value(struct ct_value *ctv) {
+	ctv->seq = 0;
+	ctv->fast_action = PKT_ACT_CONTINUE;
+	ctv->chlo_state = MEM_ERROR;
+	ctv->sni_action = SNI_APPROVE;
+	for (int i = 0; i < CT_SEQ_WINSIZE; i++) {
+		// Clang replaces this with memset by default
+		asm volatile(
+			"r1 = %[buf]\n\t"
+			"r1 += %[idx]\n\t"
+			"*(u8 *)(r1 + 0) = 0\n\t"
+			:: [buf]"r"(ctv->buf),
+				[idx]"r"(i)
+			: "r1"
+		);
+	}
+}
 
-	if (pktd->ltd.tcph.syn) {
+static __inline int tcp_ctv_update(struct packet_data *pktd, struct ct_value *ctv) {
+	int ret;
+
+	if (pktd->ltd.transport_type != TCP)
+		unreachable;
+	u32 seq = bpf_ntohl(pktd->ltd.tcph.seq);
+
+	if (ctv->seq >= seq) {
+		u32 otdiff = ctv->seq - seq;
+		otdiff += 1;
+		bpf_tt_printk("otdiff %d", otdiff);
+		if (otdiff > CT_SEQ_WINSIZE) {
+			return -1;
+		}
+
+		pktd->ltd.payload_offset += otdiff;
+		seq += otdiff;
+	}
+
+	u32 seq_diff = seq - ctv->seq;
+	u16 seq_offset = seq_diff - 1;
+	if (seq_offset >= CT_SEQ_WINSIZE) {
+		return -1;
+	}
+	exchange_tls_flags(pktd, ctv);
+
+	struct cbl_cts cbts = {	
+		.dst = ctv,
+		.dst_off = seq_offset,
+	};
+
+	if (pktd->ltd.tcph.urg) {
+		cbts.urg = bpf_ntohs(pktd->ltd.tcph.urg_ptr);
+	}
+
+	if (pktd->pkt.type == SKB_PKT) {
+		struct ct_value *ctvb;
+
+		if (pktd->pkt.skb->len < pktd->ltd.payload_offset) {
+			ret = -1;
+			goto step_out;
+		}
+		u32 copy_len = pktd->pkt.skb->len - pktd->ltd.payload_offset;
+		u32 lbuf_len = CT_SEQ_WINSIZE - seq_offset;
+		if (copy_len > lbuf_len) {
+			copy_len = lbuf_len;
+		}
+
+		if (copy_len == 0) {
+			ret = -1;
+			goto step_out;
+		}
+
+		ctvb = bpf_map_lookup_elem(&ct_value_storage, &PCP_KEY);
+		if (ctvb == NULL) {
+			// should be unreachable
+			bpf_printk("FATAL: Cannot get value storage");
+
+			ret = -1;
+			goto step_out;
+		}
+		ret = bpf_skb_load_bytes(
+			pktd->pkt.skb, pktd->ltd.payload_offset, ctvb->buf, copy_len);
+		if (ret) {
+			bpf_printk("FATAL: ctvb copy error %d", ret);
+		} else {
+			bpf_printk("Copied %d", copy_len);
+		}
+		
+		cbts.type = CBL_TYPE_CT;
+		cbts.src = ctvb;
+		cbts.len = copy_len;
+		cbts.src_off = 0;
+
+		copy_ctvs_with_offset(cbts);
+	} else {
+		u64 dstart = pktd->pkt.xdp->data;
+		u64 dend = pktd->pkt.xdp->data_end;
+		u32 doff = pktd->ltd.payload_offset;
+		if (doff > 0x0fff) {
+			ret = -1;
+			goto step_out;
+		}
+
+		dstart += doff;
+		if (dstart > dend) {
+			ret = -1;
+			goto step_out;
+		}
+
+		u32 copy_len = dend - dstart;
+		u32 lbuf_len = CT_SEQ_WINSIZE - seq_offset;
+		if (copy_len > lbuf_len) {
+			copy_len = lbuf_len;
+		}
+
+		cbts.type = CBL_TYPE_XDP;
+		cbts.ctx = pktd->pkt.xdp;
+		cbts.src_off = pktd->ltd.payload_offset;
+		cbts.len = copy_len;
+
+		copy_ctvs_with_offset(cbts);
+	}
+
+step_out:
+	if (cbts.urg) {
+		ctv->seq += 1;
+	}
+
+	return ret;
+}
+
+static __inline enum pkt_action tcp_process_conntrack(struct packet_data *pktd)
+{	
+	int ret;
+	enum pkt_action act;
+
+	struct ct_entry cte = build_ct_entry(pktd);
+
+	if (pktd->ltd.transport_type != TCP)
+		unreachable;
+
+	u32 seq = cte.tpe.seq_hash;
+	u32 seqp = seq / CT_SEQ_WINSIZE;
+	cte.tpe.seq_hash = seqp;
+	
+	if (pktd->ltd.tcph.syn && !pktd->ltd.tcph.ack) {
+		// Connection initiation
 		struct ct_value *ctv;
 		ctv = bpf_map_lookup_elem(&ct_value_storage, &PCP_KEY);
 		if (ctv == NULL) {
@@ -213,35 +365,17 @@ static __inline enum pkt_action tcp_process_conntrack(struct packet_data *pktd)
 			return PKT_ACT_CONTINUE;
 		}
 	
-
+		initialize_ct_value(ctv);
 		ctv->seq = seq;
-		ctv->fast_action = PKT_ACT_CONTINUE;
-		ctv->chlo_state = MEM_ERROR;
-		ctv->sni_action = SNI_APPROVE;
-		for (int i = 0; i < CT_SEQ_WINSIZE; i++) {
-			// Clang replaces this with memset by default
-			asm volatile(
-				"r1 = %[buf]\n\t"
-				"r1 += %[idx]\n\t"
-				"*(u8 *)(r1 + 0) = 0\n\t"
-				:: [buf]"r"(ctv->buf),
-					[idx]"r"(i)
-				: "r1"
-			);
+
+		bpf_printk("TCP Client SYN", seq);
+		ret = bpf_map_update_elem(&ct_map, &cte, ctv, BPF_ANY);
+
+		if (ret) {
+			bpf_printk("Failed to extend map: %d", ret);
 		}
-
-		// Connection initiation
-		if (!pktd->ltd.tcph.ack) {
-			bpf_printk("TCP Client SYN", seq);
-			ret = bpf_map_update_elem(&ct_map, &cte, ctv, BPF_ANY);
-
-			if (ret) {
-				bpf_printk("Failed to extend map: %d", ret);
-			}
-
-		} else {
-			bpf_printk("TCP Server SYN ACK", seq);
-		}
+	} else if (pktd->ltd.tcph.syn) {
+		bpf_printk("TCP Server SYN ACK", seq);
 	} else {
 		// Try to find an existing connection
 		struct ct_value *ctv = bpf_map_lookup_elem(&ct_map, &cte);	
@@ -252,104 +386,15 @@ static __inline enum pkt_action tcp_process_conntrack(struct packet_data *pktd)
 
 		if (ctv == NULL) {
 			return PKT_ACT_CONTINUE;
-		}
-
-
-		if (ctv->seq >= seq) {
-			u32 otdiff = ctv->seq - seq;
-			otdiff += 1;
-			bpf_tt_printk("otdiff %d", otdiff);
-			if (otdiff > CT_SEQ_WINSIZE) {
-				return PKT_ACT_CONTINUE;
-			}
-
-			pktd->ltd.payload_offset += otdiff;
-			seq += otdiff;
-		}
-
-		u32 seq_diff = seq - ctv->seq;
-		u16 seq_offset = seq_diff - 1;
-		if (seq_offset >= CT_SEQ_WINSIZE) {
-			return PKT_ACT_CONTINUE;
-		}
+		}	
 
 		if (ctv->fast_action != PKT_ACT_CONTINUE) {
 			bpf_printk("Fast action %d", ctv->fast_action);
 			return ctv->fast_action;
 		}
 
-		exchange_tls_flags(pktd, ctv);
-
-		struct cbl_cts cbts = {	
-			.dst = ctv,
-			.dst_off = seq_offset,
-		};
-
-		if (pktd->ltd.tcph.urg) {
-			cbts.urg = bpf_ntohs(pktd->ltd.tcph.urg_ptr);
-		}
-
-		if (pktd->pkt.type == SKB_PKT) {
-			struct ct_value *ctvb;
-
-			if (pktd->pkt.skb->len < pktd->ltd.payload_offset) {
-				return PKT_ACT_CONTINUE;
-			}
-			u32 copy_len = pktd->pkt.skb->len - pktd->ltd.payload_offset;
-			u32 lbuf_len = CT_SEQ_WINSIZE - seq_offset;
-			if (copy_len > lbuf_len) {
-				copy_len = lbuf_len;
-			}
-
-			if (copy_len == 0) {
-				return PKT_ACT_CONTINUE;
-			}
-
-			ctvb = bpf_map_lookup_elem(&ct_value_storage, &PCP_KEY);
-			if (ctvb == NULL) {
-				// should be unreachable
-				bpf_printk("FATAL: Cannot get value storage");
-				return PKT_ACT_CONTINUE;
-			}
-			ret = bpf_skb_load_bytes(pktd->pkt.skb, pktd->ltd.payload_offset, ctvb->buf, copy_len);
-			if (ret) {
-				bpf_printk("FATAL: ctvb copy error %d", ret);
-			} else {
-				bpf_printk("Copied %d", copy_len);
-			}
-			
-			cbts.type = CBL_TYPE_CT;
-			cbts.src = ctvb;
-			cbts.len = copy_len;
-			cbts.src_off = 0;
-
-			copy_ctvs_with_offset(cbts);
-		} else {
-			u64 dstart = pktd->pkt.xdp->data;
-			u64 dend = pktd->pkt.xdp->data_end;
-			u32 doff = pktd->ltd.payload_offset;
-			if (doff > 0x0fff) {
-				return PKT_ACT_CONTINUE;
-			}
-
-			dstart += doff;
-			if (dstart > dend) {
-				return PKT_ACT_CONTINUE;
-			}
-
-			u32 copy_len = dend - dstart;
-			u32 lbuf_len = CT_SEQ_WINSIZE - seq_offset;
-			if (copy_len > lbuf_len) {
-				copy_len = lbuf_len;
-			}
-
-			cbts.type = CBL_TYPE_XDP;
-			cbts.ctx = pktd->pkt.xdp;
-			cbts.src_off = pktd->ltd.payload_offset;
-			cbts.len = copy_len;
-
-			copy_ctvs_with_offset(cbts);
-		}
+		//ctv update
+		tcp_ctv_update(pktd, ctv);
 
 		struct pkt pkt = {
 			.type = CT_PKT,
@@ -358,10 +403,6 @@ static __inline enum pkt_action tcp_process_conntrack(struct packet_data *pktd)
 		act = process_tls(pkt, pktd, 0);
 		if (act == PKT_ACT_DROP) {
 			ctv->fast_action = act;
-		}
-
-		if (cbts.urg) {
-			ctv->seq += 1;
 		}
 
 		return act;
